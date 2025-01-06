603f9bfcd5d87847dd696820e6eec425bfdb21f6
Mdowel <150044412+Mdowel@users.noreply.github.com>
2025-01-06 16:00:58 -0500

    add result determination

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const rockEl = document.getElementById('rock')
-const paperEl = document.getElementById('paper')
-const scissorsEl = document.getElementById('scissors')
+const playerBtns = document.querySelectorAll('button')
 const playerSelectionEl = document.getElementById('your-selection')
 const houseSelectionEl = document.getElementById('house-selection')
+const resultEl = document.getElementById('result')
 
 const rockImg = document.createElement("img") 
-const paperImg = document.createElement("img") 
-const scissorsImg = document.createElement("img") 
 rockImg.src = 'images/icon-rock.svg'
+
+const paperImg = document.createElement("img") 
 paperImg.src = 'images/icon-paper.svg'
+
+const scissorsImg = document.createElement("img") 
 scissorsImg.src = 'images/icon-scissors.svg'
+
+let playerSelection = ''
 
 const houseOptions = [
     {
@@ -32,35 +35,52 @@ function getRandomIndex(array) {
     return randomIndex
 }
 
+playerBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        // console.log(`${btn.id} clicked`)
+        playerSelectionEl.innerHTML = ''
+        if (btn.id === 'rock') {
+            playerSelectionEl.append('Rock', rockImg)
+            playerSelection = 'Rock'
+
+        } else if (btn.id === 'paper') {
+            playerSelectionEl.append('Paper', paperImg)
+            playerSelection = 'Paper'
+
+        } else if (btn.id === 'scissors') {
+            playerSelectionEl.append('Scissors', scissorsImg)
+            playerSelection = 'Scissors'
+
+        }
+        getHouseSelection()
+    })
+})
+
 function getHouseSelection() {
     const randomIndex = getRandomIndex(houseOptions)
     const houseSelection = houseOptions[randomIndex]
-    
+
     // clone img so player and house each have their own img elements
     const clonedImg = houseSelection.img.cloneNode()
-
+    clonedImg.classList.add('house-selection-img')
+    // add selection to DOM
     houseSelectionEl.innerHTML = ''
     houseSelectionEl.append(houseSelection.name, clonedImg)
-    console.log("House:", houseSelection.name)
+
+
+    if (playerSelection === houseSelection.name) {
+        resultEl.innerText = "Tie!"
+    } else if (playerSelection === 'Rock' && houseSelection.name === 'Paper') {
+        resultEl.innerText = 'You Lose 😞'
+    } else if (playerSelection === 'Rock' && houseSelection.name === 'Scissors'){
+        resultEl.innerText = 'You Win! 😀'
+    } else if (playerSelection === 'Paper' && houseSelection.name === 'Rock'){
+        resultEl.innerText = 'You Win! 😀'
+    } else if (playerSelection === 'Paper' && houseSelection.name === 'Scissors'){
+        resultEl.innerText = 'You Lose 😞'
+    } else if (playerSelection === 'Scissors' && houseSelection.name === 'Rock'){
+        resultEl.innerText = 'You Lose 😞'
+    } else if (playerSelection === 'Scissors' && houseSelection.name === 'Paper'){
+        resultEl.innerText = 'You Win! 😀'
+    }
 }
-
-rockEl.addEventListener('click', () => {
-    console.log('rock clicked')
-    playerSelectionEl.innerHTML = ''
-    playerSelectionEl.append('Rock', rockImg)
-    getHouseSelection()
-})
-
-paperEl.addEventListener('click', () => {
-    console.log('paper clicked')
-    playerSelectionEl.innerHTML = ''
-    playerSelectionEl.append('Paper', paperImg)
-    getHouseSelection()
-})
-
-scissorsEl.addEventListener('click', () => {
-    console.log('scissors clicked')
-    playerSelectionEl.innerHTML = ''
-    playerSelectionEl.append('Scissors', scissorsImg)
-    getHouseSelection()
-})
